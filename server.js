930e7162fc0b943a01f3bf3845d706faa7bce40e
CapNemo <46331608+Hone12345678@users.noreply.github.com>
2022-01-07 23:20:52 -0700

    completed front end integration

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const {animals} = require('./data/animals');
+const { response } = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -104,6 +107,24 @@ app.get('/api/animals', (req, res) => {
     }
   });
   
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
 app.post('/api/animals', (req,res) => {
     // req.body is where our incoming content will be
     // set id base on what the next index of the array will be
